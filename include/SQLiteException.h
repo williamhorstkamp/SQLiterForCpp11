e35f5471d08d2ad4844ae1bf603f6a592a0283af
William Horstkamp <william.horstkamp@gmail.com>
2015-01-23 23:56:00 -0800

    Added classes and functions to SQLiter namespace

--- include/SQLiteException.h
+++ include/SQLiteException.h
@@ -8,27 +8,25 @@
 
 #include <stdexcept>
 
-#ifndef SQLITEEXCEPTION_H
-#define SQLITEEXCEPTION_H
+namespace SQLiter{
 
-/**
- *  Simple exception class wraps an error message, typically one derived from
- *  an SQLite3 result code, but also others such as file i/o exceptions thrown
- *  by database create and open functions.
- */
-
-class SQLiteException : public std::runtime_error {
-public:
     /**
-     *  Constructor accepts a pointer to a C string containing the error
-     *  message. Hands that error message to it's parent constructor.
-     *
-     *  @param errMsg - Null terminated C String containing the error message
-     *      to built an SQLiteException around.
-     *
-     *  @return - SQLiteException wrapping the error message to be thrown.
+     *  Simple exception class wraps an error message, typically one derived from
+     *  an SQLite3 result code, but also others such as file i/o exceptions thrown
+     *  by database create and open functions.
      */
-    SQLiteException(const char *errMsg) : std::runtime_error(errMsg) {};
-};
 
-#endif
+    class SQLiteException : public std::runtime_error {
+    public:
+        /**
+         *  Constructor accepts a pointer to a C string containing the error
+         *  message. Hands that error message to it's parent constructor.
+         *
+         *  @param errMsg - Null terminated C String containing the error message
+         *      to built an SQLiteException around.
+         *
+         *  @return - SQLiteException wrapping the error message to be thrown.
+         */
+        SQLiteException(const char *errMsg) : std::runtime_error(errMsg) {};
+    };
+}
--- include/ValueHandler.h
+++ include/ValueHandler.h
@@ -10,83 +10,82 @@
 
 #include <sqlite3.h>
 
-#ifndef VALUEHANDLER_H
-#define VALUEHANDLER_H
-
-/**
- *  Wrapper for the various types of values that can be stored in an SQlite3
- *  field. Used as the return type of StatementHandler.getColumn(const int).
- *  Class consists of a series of inline functions used for implicit type 
- *  coversion in the form of varType var = StatementHandler.getColumn(const int)
- *  
- *  **CASTING TO THE WRONG TYPE IS UNDEFINED AND OFTEN UNSAFE - BE WARNED**
- *  **CHECK THE COL TYPE WITH StatementHandler.getType(const int) BEFOREHAND**
- */
-class ValueHandler {
-private:
-    sqlite3_stmt *stmt;
-    int colNum;
-public:
+namespace SQLiter {
 
     /**
-     *  Constructor accepts pointer to statement and column number to build a
-     *  representation of the values currently stored in that field.
+     *  Wrapper for the various types of values that can be stored in an SQlite3
+     *  field. Used as the return type of StatementHandler.getColumn(const int).
+     *  Class consists of a series of inline functions used for implicit type
+     *  coversion in the form of varType var = StatementHandler.getColumn(const int)
      *
-     *  @param inputStmt - Pointer to the statement that contains the value
-     *  @param inputColNum - Integer representing the column that contains
-     *      the value.
-     *
-     *  @return - ValueHandler object that wraps the value.
+     *  **CASTING TO THE WRONG TYPE IS UNDEFINED AND OFTEN UNSAFE - BE WARNED**
+     *  **CHECK THE COL TYPE WITH StatementHandler.getType(const int) BEFOREHAND**
      */
-    ValueHandler(sqlite3_stmt *inputStmt, const int inputColNum) :
-        stmt(inputStmt), colNum(inputColNum) {};
+    class ValueHandler {
+    private:
+        sqlite3_stmt *stmt;
+        int colNum;
+    public:
 
-    /**
-    *   Integer conversion operator for implicit type coversion to integer
-    *
-    *   @return - Integer value contained within the given column and statement
-    */
-    inline operator int() const  {
-        return sqlite3_column_int(stmt, colNum);
-    }
+        /**
+         *  Constructor accepts pointer to statement and column number to build a
+         *  representation of the values currently stored in that field.
+         *
+         *  @param inputStmt - Pointer to the statement that contains the value
+         *  @param inputColNum - Integer representing the column that contains
+         *      the value.
+         *
+         *  @return - ValueHandler object that wraps the value.
+         */
+        ValueHandler(sqlite3_stmt *inputStmt, const int inputColNum) :
+            stmt(inputStmt), colNum(inputColNum) {};
 
-    /**
-    *   Int64 conversion operator for implicit type coversion to int64
-    *
-    *   @return - sqlite_int64 value contained within the given column and
-    *       statement
-    */
-    inline operator sqlite3_int64() const {
-        return sqlite3_column_int64(stmt, colNum);
-    }
+        /**
+        *   Integer conversion operator for implicit type coversion to integer
+        *
+        *   @return - Integer value contained within the given column and statement
+        */
+        inline operator int() const  {
+            return sqlite3_column_int(stmt, colNum);
+        }
 
-    /**
-    *  Blob conversion operator for implicit type coversion to blob
-    *
-    *   @return - Pointer to sqlite3 blob
-    */
-    inline operator const void *() const  {
-        return sqlite3_column_blob(stmt, colNum);
-    }
+        /**
+        *   Int64 conversion operator for implicit type coversion to int64
+        *
+        *   @return - sqlite_int64 value contained within the given column and
+        *       statement
+        */
+        inline operator sqlite3_int64() const {
+            return sqlite3_column_int64(stmt, colNum);
+        }
 
-    /**
-    *   Double conversion operator for implicit type coversion to floating 
-    *   point
-    *
-    *   @return - Double value contained within the given column and statement
-    */
-    inline operator double() const {
-        return sqlite3_column_double(stmt, colNum);
-    }
+        /**
+        *  Blob conversion operator for implicit type coversion to blob
+        *
+        *   @return - Pointer to sqlite3 blob
+        */
+        inline operator const void *() const  {
+            return sqlite3_column_blob(stmt, colNum);
+        }
 
-    /**
-    *   String conversion operator for implicit type coversion to C string
-    *
-    *   @return - Pointer to null-terminated C String
-    */
-    inline operator const char *() const {
-        return (const char*)sqlite3_column_text(stmt, colNum);
-    }
+        /**
+        *   Double conversion operator for implicit type coversion to floating
+        *   point
+        *
+        *   @return - Double value contained within the given column and statement
+        */
+        inline operator double() const {
+            return sqlite3_column_double(stmt, colNum);
+        }
 
-};
-#endif
+        /**
+        *   String conversion operator for implicit type coversion to C string
+        *
+        *   @return - Pointer to null-terminated C String
+        */
+        inline operator const char *() const {
+            return (const char*)sqlite3_column_text(stmt, colNum);
+        }
+
+    };
+}
