5c60c111c3fb1c14542747050c3aa0c95c16fca6
William Horstkamp <william.horstkamp@gmail.com>
2015-01-23 19:53:25 -0800

    First public version
    First public release of an SQLite3 wrapper for C++11 I've been working
    on and with. Stable and useable as is, with more improvements coming
    down the pipe.

---
+++ src/SQLiteHandler.cpp
@@ -0,0 +1,96 @@
+/**
+*  SQLiteHandler.h
+*  Provides a portable SQLite3 interface to be used with the StatementHandler
+*
+*  @author William Horstkamp
+*  @version 0.5
+*/
+
+/**
+ *  SQLiter For C++11 is an SQLite3 wrapper with C++11 features.
+ *  Copyright (C) 2015 William Horstkamp
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a
+ *	copy of this software and associated documentation files (the "Software"),
+ *	to deal in the Software without restriction, including without limitation
+ *	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *	and/or sell copies of the Software, and to permit persons to whom the
+ *	Software is furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in
+ *	all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *	DEALINGS IN THE SOFTWARE.
+ */
+
+#include "SQLiteHandler.h"
+
+SQLiteHandler::SQLiteHandler(const char *location) {
+   forceOpenDatabase(location);
+}
+
+SQLiteHandler::~SQLiteHandler() {
+    destroyStatements();
+    db.reset();
+}
+
+StatementHandler *SQLiteHandler::prepareStatement(const char *key, const char *stmtStr) {
+    stmts.insert(std::make_pair(key, std::unique_ptr<StatementHandler>(new StatementHandler(db.get(), stmtStr))));
+    return getStatement(key);
+}
+
+
+void SQLiteHandler::destroyStatements() {
+    stmts.clear();
+}
+
+StatementHandler *SQLiteHandler::getStatement(const char *key) {
+    return stmts.at(key).get();
+}
+
+void SQLiteHandler::forceOpenDatabase(const char *location) {
+    sqlite3 *connection = nullptr;
+    result(sqlite3_open(location, &connection));
+    db.reset(connection);
+}
+
+void SQLiteHandler::openDatabase(const char *location) {
+    if (fileExists(location)) {
+        sqlite3 *connection = nullptr;
+        result(sqlite3_open(location, &connection));
+        db.reset(connection);
+    } else {
+        throw SQLiteException("File Does Not Exist");
+    }
+}
+
+void SQLiteHandler::createDatabase(const char *location) {
+    if (!fileExists(location)) {
+        sqlite3 *connection = nullptr;
+        result(sqlite3_open(location, &connection));
+        db.reset(connection);
+    } else {
+        throw SQLiteException("File Already Exists");
+    }
+}
+
+void SQLiteHandler::closeDatabase() {
+    destroyStatements();
+    db.reset();
+}
+
+int SQLiteHandler::rawExec(const char *stmtStr) {
+    result(sqlite3_exec(db.get(), stmtStr, NULL, NULL, NULL));
+    return sqlite3_changes(db.get());
+}
+
+void SQLiteHandler::result(const int resCode) {
+    if (resCode != SQLITE_OK)
+        throw SQLiteException(sqlite3_errmsg(db.get()));
+}
---
+++ src/StatementHandler.cpp
@@ -0,0 +1,117 @@
+/**
+ *  StatementHandler.cpp
+ *  Provides a wrapper for a unique_ptr<sqlite3_stmt> that both
+ *  cleanly prepares the statement and manages the lifetime of it
+ *  with the bonus of providing a convenient interface
+ *
+ *  @author William Horstkamp
+ *  @version 0.5
+ */
+
+/**
+ *  SQLiter For C++11 is an SQLite3 wrapper with C++11 features.
+ *  Copyright (C) 2015 William Horstkamp
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a
+ *	copy of this software and associated documentation files (the "Software"),
+ *	to deal in the Software without restriction, including without limitation
+ *	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *	and/or sell copies of the Software, and to permit persons to whom the
+ *	Software is furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in
+ *	all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *	DEALINGS IN THE SOFTWARE.
+ */
+
+#include <cstring>
+#include "SQLiteException.h"
+#include "StatementHandler.h"
+
+StatementHandler::~StatementHandler() {
+    stmt.reset();
+}
+
+StatementHandler::StatementHandler(sqlite3 *db, const char *stmtStr) {
+    sqlite3_stmt *prepStmt;
+    sqlite3_prepare_v2(db, stmtStr, strlen(stmtStr), &prepStmt, nullptr);
+    stmt = std::unique_ptr<sqlite3_stmt, Closesqlite3_stmt>(prepStmt);
+}
+
+void StatementHandler::bind(const int var, const char *input) {
+    sqlite3_bind_text(stmt.get(), var, input, strlen(input), SQLITE_TRANSIENT);
+}
+
+void StatementHandler::bind(const int var, const int input) {
+    sqlite3_bind_int(stmt.get(), var, input);
+}
+
+void StatementHandler::bind(const int var, const double input) {
+    sqlite3_bind_double(stmt.get(), var, input);
+}
+
+void StatementHandler::bind(const int var, const void *input, const int size) {
+    sqlite3_bind_blob(stmt.get(), var, input, size, SQLITE_TRANSIENT);
+}
+
+void StatementHandler::bindNull(const int var) {
+    sqlite3_bind_null(stmt.get(), var);
+}
+
+const int StatementHandler::getType(const int column) {
+    int typeNum = sqlite3_column_type(stmt.get(), column);
+    return (typeNum >= SQLITE_INTEGER && typeNum <= SQLITE_NULL ? typeNum : 0);
+}
+
+const int StatementHandler::getSize(const int column) {
+    return sqlite3_column_bytes(stmt.get(), column);
+}
+
+std::string StatementHandler::getString(const int column) {
+    if (getType(column) == SQLITE_TEXT) { 
+        return std::string((char *)sqlite3_column_text(stmt.get(), column));
+    } throw SQLiteException("Column doesn't contain a string");
+}
+
+int StatementHandler::getInt(const int column) {
+    if (getType(column) == SQLITE_INTEGER) {
+        return sqlite3_column_int(stmt.get(), column);
+    } throw SQLiteException("Column doesn't contain a int");
+}
+
+double StatementHandler::getDouble(const int column) {
+    if (getType(column) == SQLITE_FLOAT) {
+        return sqlite3_column_double(stmt.get(), column);
+        
+    } throw SQLiteException("Column doesn't contain a float");
+}
+
+const void *StatementHandler::getBlob(const int column) {
+    if (getType(column) == SQLITE_BLOB) {
+        return sqlite3_column_blob(stmt.get(), column);
+    } throw SQLiteException("Column doesn't contain a blob");
+}
+
+ValueHandler StatementHandler::getColumn(const int column) {
+    return ValueHandler(stmt.get(), column);
+}
+
+bool StatementHandler::step() {
+    return (sqlite3_step(stmt.get()) == SQLITE_ROW);
+}
+
+ void StatementHandler::reset() {
+     sqlite3_reset(stmt.get());
+ }
+
+ void StatementHandler::clear() {
+     sqlite3_clear_bindings(stmt.get());
+ }
+
